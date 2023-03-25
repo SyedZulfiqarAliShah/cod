a200bb3982a7fa5f881f488d31b44fdbb8a2d6d4
Zulfiqar <zulfiqar@uetpeshawar.edu.pk>
2023-03-25 10:28:44 +0500

    Initial commit today on 25-3-2023

---
+++ learn.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <style>
+        h2 {    text-align: center; }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <h2>Learning PHP</h2>
+    <?php
+    echo "This is really a fun to play with code.";
+    ?>
+</body>
+</html>
